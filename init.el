aed71782966e4de1c82df50220932526aed49abe
zq-cmd <zq_cmd@163.com>
2021-02-08 12:24:11 +0800

    remove shell snippets

--- init.el
+++ init.el
@@ -86,6 +86,8 @@
 
 (helm-mode 1)
 
+(remove-hook 'helm-cleanup-hook 'helm-handle-winner-boring-buffers)
+
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "C-x b") 'helm-buffers-list)
