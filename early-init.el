c1eead6747e21ac5437e845b03c25c039efda202
zq-cmd <zq_cmd@163.com>
2021-04-26 02:00:14 +0800

    update

--- early-init.el
+++ early-init.el
@@ -1,12 +1,12 @@
 (setq gc-cons-percentage 0.6
       gc-cons-threshold most-positive-fixnum
-      file-name-handler-alist-init file-name-handler-alist
+      +file-name-handler-alist file-name-handler-alist
       file-name-handler-alist nil)
 
 (add-hook 'after-init-hook
           (lambda ()
             (setq gc-cons-percentage 0.1
                   gc-cons-threshold 20000000
-                  file-name-handler-alist file-name-handler-alist-init)))
+                  file-name-handler-alist +file-name-handler-alist)))
 
 (setq package-enable-at-startup nil)
