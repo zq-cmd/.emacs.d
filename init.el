b9139a06a6db41751bd15aee8c7a898e08154693
zq-cmd <zq_cmd@163.com>
2021-05-18 19:28:03 +0800

    +occur, +completion, remove selectrum

--- init.el
+++ init.el
@@ -12,8 +12,6 @@
 (menu-bar-mode -1)
 (blink-cursor-mode -1)
 
-(load-theme 'modus-vivendi)
-
 
 (setq confirm-kill-emacs 'y-or-n-p
       vc-handled-backends '(Git)
@@ -65,9 +63,6 @@
 
 (setq isearch-lazy-count t)
 
-(define-key isearch-mode-map (kbd ".") 'isearch-repeat-forward)
-(define-key isearch-mode-map (kbd ",") 'isearch-repeat-backward)
-
 (setq view-read-only t)
 
 (with-eval-after-load 'view
@@ -93,31 +88,154 @@
 (setq input-method-function '+input-method-function)
 
 
-(defun +selectrum-refine (query candidates)
-  (let ((regexp (string-join (split-string query) ".*")))
-    (condition-case error
-        (seq-filter (lambda (candidate) (string-match-p regexp candidate))
-                    candidates)
-      (invalid-regexp nil))))
+(defvar +occur-alive-p nil)
+(defvar +occur-timer nil)
 
-(defun +selectrum-highlight (query candidates)
-  (let ((regexp (string-join (split-string query) ".*")))
-    (condition-case error
-        (mapcar (lambda (candidate)
-                  (if (string-match regexp candidate)
-                      (let ((copyed-candidate (copy-sequence candidate)))
-                        (apply 'font-lock-prepend-text-property
-                               `(,@(match-data) face match ,copyed-candidate))
-                        copyed-candidate)
-                    candidate))
-                candidates)
-      (invalid-regexp candidates))))
+(defun +occur-update ()
+  (if +occur-alive-p
+      (let ((regexp (minibuffer-contents))
+            (buffer (get-buffer "*Occur*")))
+        (when (and (not (string-empty-p regexp))
+                   (not (and buffer
+                             (equal regexp
+                                    (with-current-buffer buffer
+                                      occur-highlight-regexp)))))
+          (with-selected-window (minibuffer-selected-window)
+            (condition-case error
+                (occur regexp)
+              (invalid-regexp nil)))))
+    (cancel-timer +occur-timer)))
 
-(setq selectrum-refine-candidates-function '+selectrum-refine
-      selectrum-highlight-candidates-function '+selectrum-highlight)
+(defun +occur ()
+  (interactive)
+  (let ((+occur-alive-p t)
+        (buffer (get-buffer "*Occur*")))
+    (when buffer
+      (display-buffer buffer))
+    (setq +occur-timer (run-with-idle-timer 0.5 t '+occur-update))
+    (read-from-minibuffer "occur: ")
+    (unless buffer
+      (setq buffer (get-buffer "*Occur*"))
+      (when buffer
+        (display-buffer buffer)))))
 
-(selectrum-mode 1)
+(global-set-key (kbd "M-#") '+occur)
 
+
+(require 'subr-x)
+
+(defvar +completion-collection nil)
+(defvar +completion-window nil)
+(defvar +completion-timer nil)
+
+(defun +completion-update (&optional query)
+  (interactive)
+  (let ((query (or query (minibuffer-contents))))
+    (if +completion-window
+        (with-selected-window +completion-window
+          (unless (equal query header-line-format)
+            (setq header-line-format query)
+            (erase-buffer)
+            (if (string-empty-p query)
+                (let ((count 0)
+                      (current +completion-collection))
+                  (while (and (< count 50) current)
+                    (insert (car current) "\n")
+                    (setq count (1+ count)
+                          current (cdr current))))
+              (let ((regexp (string-join (split-string query) ".*")))
+                (condition-case error
+                    (let ((count 0)
+                          (current +completion-collection))
+                      (while (and (< count 50) current)
+                        (when (string-match regexp (car current))
+                          (let ((match-data (match-data))
+                                (copyed (copy-sequence (car current))))
+                            (font-lock-prepend-text-property
+                             (car match-data) (cadr match-data) 'face 'match copyed)
+                            (insert copyed "\n"))
+                          (setq count (1+ count)))
+                        (setq current (cdr current))))
+                  (invalid-regexp nil))))
+            (goto-char (point-min))
+            (hl-line-highlight)))
+      (cancel-timer +completion-timer))))
+
+(defun +completion-next ()
+  (interactive)
+  (with-selected-window +completion-window
+    (next-line)
+    (hl-line-highlight)))
+
+(defun +completion-prev ()
+  (interactive)
+  (with-selected-window +completion-window
+    (previous-line)
+    (hl-line-highlight)))
+
+(defun +completion-complete ()
+  (interactive)
+  (delete-region (line-beginning-position) (line-end-position))
+  (insert (with-selected-window +completion-window
+            (buffer-substring-no-properties (line-beginning-position)
+                                            (line-end-position)))))
+
+(defun +completion-exit-minibuffer ()
+  (interactive)
+  (+completion-complete)
+  (exit-minibuffer))
+
+(defvar +completion-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-j") 'exit-minibuffer)
+    (define-key map (kbd "C-r") '+completion-update)
+    (define-key map (kbd "C-n") '+completion-next)
+    (define-key map (kbd "C-p") '+completion-prev)
+    (define-key map (kbd "TAB") '+completion-complete)
+    (define-key map (kbd "RET") '+completion-exit-minibuffer)
+    (make-composed-keymap map minibuffer-local-map)))
+
+(defun +completion-read (collection)
+  (save-window-excursion
+    (let* ((+completion-collection collection)
+           (+completion-window
+            (with-selected-window (if (window-minibuffer-p)
+                                      (minibuffer-selected-window)
+                                    (selected-window))
+              (switch-to-buffer-other-window "*+completion+*")
+              (hl-line-mode 1)
+              (selected-window))))
+      (+completion-update "")
+      (setq +completion-timer (run-with-idle-timer 0.5 t '+completion-update))
+      (unwind-protect
+          (read-from-minibuffer "complete: " nil +completion-map)
+        (kill-buffer (window-buffer +completion-window))))))
+
+(defun +completion-in-region (beg end collection predicate)
+  (completion-in-region-mode -1)
+  (let* ((enable-recursive-minibuffers t)
+         (prefix (buffer-substring beg end))
+         (metadata (completion-metadata prefix collection predicate))
+         (boundary (+ beg (car (completion-boundaries prefix collection predicate ""))))
+         (choices (nconc (completion-all-completions
+                          prefix collection predicate (length prefix))
+                         nil))
+         (choice (cond ((null choices) nil)
+                       ((null (cdr choices)) (car choices))
+                       (t (+completion-read choices)))))
+    (when choice
+      (delete-region boundary end)
+      (insert (substring-no-properties choice)))))
+
+(defun +completion-at-point ()
+  (interactive)
+  (let ((completion-in-region-function '+completion-in-region))
+    (call-interactively (lookup-key `(,(current-local-map) ,(current-global-map))
+                                    (kbd "TAB")))))
+
+(global-set-key (kbd "<f2>") '+completion-at-point)
+
+
 (defun +indent-or-filter (command)
   (if (or (use-region-p)
           (<= (current-column)
